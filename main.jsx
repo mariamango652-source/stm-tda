5758f4f647cc40d3da72b19e35e3f300275f073b
mariamango652-source <mariamango652@gmail.com>
2026-06-23 13:20:39 +0300

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,4 +1,4 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './Src/App.jsx'
+import App from './src/src/App.jsx'
 ReactDOM.createRoot(document.getElementById('root')).render(<App />)
