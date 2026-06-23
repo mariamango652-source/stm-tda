a8132a72347a8affac6917b5e5f643e82774186a
mariamango652-source <mariamango652@gmail.com>
2026-06-23 13:14:20 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
-  build: { outDir: 'dist', chunkSizeWarningLimit: 2000 },
-  root: '.'
+  root: '.',
+  build: { 
+    outDir: 'dist', 
+    chunkSizeWarningLimit: 2000,
+    rollupOptions: {
+      input: './index.html'
+    }
+  }
 })
